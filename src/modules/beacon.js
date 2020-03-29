3b5b26d41b954f53edcbf1ca6a8049be98ae5734
Mrityunjay <mrityunjay38@gmail.com>
2020-03-29 21:50:13 +0530

    Adds feature to extend themeSettings + prettier

--- src/modules/beacon.js
+++ src/modules/beacon.js
@@ -1,11 +1,15 @@
 const beacon = () => {
-    return new Promise( (resolve,reject) => {
-        const img = new Image();
-        const foolCache = new Date().getTime();
-        img.src = 'https://www.google.com/favicon.ico' + '?fool-cache=' + foolCache;
-        img.onload = () => {resolve('connected')};
-        img.onerror = () => {reject('disconnected')};
-    });
-}
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    const foolCache = new Date().getTime();
+    img.src = "https://www.google.com/favicon.ico" + "?fool-cache=" + foolCache;
+    img.onload = () => {
+      resolve("connected");
+    };
+    img.onerror = () => {
+      reject("disconnected");
+    };
+  });
+};
 
 export default beacon;
